0b7aa2ebf72ead65ff6066a9ee8fdde48724e790
JohnWL <34081873+John-WL@users.noreply.github.com>
2024-09-11 18:28:29 -0400

    style: normalizer les espaces

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-    <nav-menu></nav-menu>
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <nav-menu></nav-menu>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
 import Navigation from "@/components/Navigation";
 export default {
-  name: "app",
-  components: {
-    "nav-menu": Navigation,
-  },
+    name: "app",
+    components: {
+        "nav-menu": Navigation,
+    },
 };
 </script>
 
